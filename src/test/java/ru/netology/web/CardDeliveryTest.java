d1dcab8771daad79fbfb4670288e81fac57721a9
ynappva@redmachinegames.com <cC255NQxG>
2024-12-22 03:05:49 +0300

    test updated

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -3,22 +3,20 @@ package ru.netology.web;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.SelenideElement;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
+
+import java.time.Duration;
 import java.time.Duration.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.$;
+import static java.time.Duration.of;
 import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -33,16 +31,21 @@ class CardDeliveryTest {
     void shouldSendFormIfDataValid() {
         Selenide.open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Уфа");
-//        $("[data-test-id='date']").press(Keys.chord(Keys.CONTROL, "a"));
-//        $("[data-test-id='date']").press(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.CONTROL, "a"));
+        $("[data-test-id='date'] input").press(Keys.BACK_SPACE);
         String datePlanning = generateDate(4, "dd.MM.yyy");
         $("[data-test-id='date'] input").setValue(datePlanning);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79051369202");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
-        WebElement result = $("[data-test-id='notification']").shouldBe(Condition.visible, ofSeconds(15));
-        assertTrue(result.isDisplayed());
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + datePlanning));
+
+
+
+
 
 
 
